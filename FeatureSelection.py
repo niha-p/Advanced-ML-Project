0074e0e42c00e700b3181dd784b12cc0dc2ce641
niha-p <niharika.purbey@gmail.com>
2016-05-02 00:36:52 -0400

    Just some cleaning

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -46,7 +46,7 @@ features = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 features2 = [x for x in range(0,24)]
 features = features + features2
 
-"""
+
 print "Variance Threshold"
 sel = VarianceThreshold(threshold=(0.90 * (1 - 0.90)))
 selector=sel.fit(training[features])
@@ -79,15 +79,6 @@ for i in range(0,len(features)):
         print features[i]
 
 
-print "Lasso CV"
-clf = LassoCV()
-clf=clf.fit(training[features], training['crime'])
-model = SelectFromModel(clf, threshold=0.25,prefit=True)
-print model.get_support(indices=True)
-
-for i in range(0,len(features)):
-    if i in model.get_support(indices=True):
-        print features[i]
 
 
 print "fclassif - Select Percentile"
@@ -99,57 +90,6 @@ for i in range(0,len(features)):
     if i in selector.get_support(indices=True):
         print features[i]
 
-"""
-print "PCA"
-
-"""
-pca = PCA(n_components='mle')
-selector=pca.fit(training[features])
-print selector.get_params()
-
-for i in range(0,len(features)):
-    if i in selector.get_support(indices=True):
-        print features[i]
-"""
-
-from matplotlib.mlab import PCA
-res = PCA(training[features])
-print "weights of input vectors: %s" % res.Wt
-
-
-"""
-*******  OUTPUT  *******
-Variance Threshold
-[ 0  1  2  3  4  5  6  7 10 11 14 18]
-Select from Model - Logistic
-[ 7  8  9 10 12 13 14 16 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 41 42]
-Tree Based Feature Selection
-[17 18]
-Lasso CV
-[ 7  8  9 11 13 14 15 16 17 19 20 21 24 25 26 30 31 32 33 34 37 39 40 41 42]
-fclassif - Select Percentile
-[ 7  8  9 10 11 12 13 14 15 16 17 19 20 21 22 25 26 27 31 39 41]
-
-"""
-
-
-
-"""
-*****Doesn't work - Only for non-negative features ****
-print "chi2 - Select Percentile"
-selector = SelectPercentile(chi2, percentile=75)
-selector = selector.fit(training[features], training['crime'])
-print selector.get_support(indices=True)
-"""
-
-"""
-*****Doesn't work*****
-print "Select KBest"
-X_new = SelectKBest(chi2, k=20)
-selector=X_new.fit_transform(training[features])
-#print selector.get_support(indices=True)
-print selector.shape
-"""
 
 
 """
@@ -169,7 +109,7 @@ MISSION
 NORTHERN
 SOUTHERN
 Y
-Select from Model - Logistic
+Select from Model - Logistic Lasso
 [ 7  8  9 10 12 13 16 18 19 20 21 22 23 24 25 26 27 28 29 30 31 41 42]
 BAYVIEW
 CENTRAL
@@ -198,32 +138,6 @@ Tree Based Feature Selection
 [17 18]
 X
 Y
-Lasso CV
-[ 7  8  9 11 13 14 15 16 17 20 23 24 25 26 30 31 32 33 34 37 39 40 41 42]
-BAYVIEW
-CENTRAL
-INGLESIDE
-NORTHERN
-RICHMOND
-SOUTHERN
-TARAVAL
-TENDERLOIN
-X
-1
-4
-5
-6
-7
-11
-12
-13
-14
-15
-18
-20
-21
-22
-23
 fclassif - Select Percentile
 [ 7  8  9 10 11 12 13 14 15 16 17 19 20 21 22 24 25 26 27 31 41]
 BAYVIEW
